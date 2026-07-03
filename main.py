982f37f95e70624b0e1e33d588205b06830eda5c
Aiden Murphy <aidenmurphy@Aidens-MacBook-Pro.local>
2026-07-03 18:48:41 -0400

    added a gui and the ability to turn the notifications to all

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import cv2
 import tkinter as tk
 pg.FAILSAFE = False
 def youtube():
+    notify_on = notify.get()
     channel = line_edit.get()
     pg.moveTo(0,0)
 
@@ -26,12 +27,20 @@ def youtube():
         "var b=Array.from(document.querySelectorAll('button,span,yt-formatted-string'))"
         ".find(e=>['Subscribe','Subscribed'].includes(e.textContent.trim()));if(b)b.click();"
     )
-    pg.typewrite(js_click_script)
+    js_click_script_notify = (
+    "var s=Array.from(document.querySelectorAll('button,span,yt-formatted-string')).find(e=>['Subscribe','Subscribed'].includes(e.textContent.trim()));if(s){s.click();setTimeout(()=>{var a=Array.from(document.querySelectorAll('ytd-menu-service-item-renderer,tp-yt-paper-item,span')).find(e=>e.textContent.trim()==='All');if(a)a.click();},400);}"
+    )
+
+
+    if notify_on == False:
+        pg.typewrite(js_click_script)
+    else:
+        pg.typewrite(js_click_script_notify)
     pg.hotkey("return")
     pg.hotkey("command","option","j",interval=0.1)
 root = tk.Tk()
 root.title = "Auto Subscriber"
-root.geometry("300x200")
+root.geometry("600x400")
 
 title = tk.Label(root,text="Auto Subscriber",font=("Ariel", 16, "bold"))
 title.pack(pady=(20,10))
@@ -41,4 +50,9 @@ line_edit = tk.Entry(root, width=20,borderwidth=5,font=("Ariel",12))
 line_edit.pack()
 button = tk.Button(root,text="Subscribe",borderwidth=5,font=("Ariel", 15, "bold"), command=youtube)
 button.pack()
+label2 = tk.Label(root,text="Customization", font=("Ariel",16, "bold"))
+label2.pack(pady=(30,0))
+notify = tk.BooleanVar()
+notifications = tk.Checkbutton(root,text="Notifications On",variable=notify)
+notifications.pack()
 root.mainloop()
