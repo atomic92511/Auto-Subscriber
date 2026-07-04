3fb0110eae66b4d509a2ad234982f605bea79b21
Aiden Murphy <aidenmurphy@Aidens-MacBook-Pro.local>
2026-07-03 21:41:39 -0400

    added reading sub count and auto closing the window

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import time
 import PIL 
 import cv2
 import customtkinter as tk
+import pyperclip
 pg.FAILSAFE = False
+subcount = None
 def change_mode():
     if dark.get() == True:
         tk.set_appearance_mode("dark")
@@ -42,7 +44,20 @@ def youtube():
     else:
         pg.typewrite(js_click_script_notify)
     pg.hotkey("return")
+    js_get_count_script = (
+        "var countElem=document.querySelector('#subscriber-count, .yt-core-attributed-string, [aria-label*=\"subscribers\"]');"
+        "var cleanCount=countElem?countElem.textContent.replace(/[^0-9.KM]/g,'').trim():'Not Found';"
+        "copy(cleanCount);"  # <-- JavaScript sends the data to your system clipboard
+    )
+    pg.typewrite(js_get_count_script)
+    pg.hotkey("return",interval=0.1)
     pg.hotkey("command","option","j",interval=0.1)
+    subcount = pyperclip.paste()
+    if subcount != None:
+        subs.configure(text="You were the " + str(subcount) + " subscriber of the channel " + str(channel) + "!")
+    if auto_close.get() == True:
+        pg.hotkey("command","shift","w",interval=0.1)
+        
 root = tk.CTk()
 root.title = "Auto Subscriber"
 root.geometry("600x400")
@@ -56,6 +71,8 @@ line_edit = tk.CTkEntry(root, width=200,border_width=5,font=("Ariel",12))
 line_edit.pack()
 button = tk.CTkButton(root,text="Subscribe",border_width=5,font=("Ariel", 15, "bold"), command=youtube)
 button.pack(pady=(10,0))
+subs = tk.CTkLabel(root,text="")
+subs.pack()
 label2 = tk.CTkLabel(root,text="Customization", font=("Ariel",16, "bold"))
 label2.pack(pady=(30,0))
 notify = tk.BooleanVar()
@@ -64,4 +81,7 @@ notifications.pack()
 dark = tk.BooleanVar()
 dark_mode = tk.CTkCheckBox(root,text="Dark Mode        ",variable=dark,command=change_mode)
 dark_mode.pack(pady=(10,0))
+auto_close = tk.BooleanVar()
+autoclose = tk.CTkCheckBox(root,text="Auto Close Tab ",variable=auto_close)
+autoclose.pack(pady=(10,0))
 root.mainloop()
